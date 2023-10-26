59f4166d75662e3874959e4d951663f0cc7cb4b2
storezhang <storezhang@gmail.com>
2023-10-26 13:38:50 +0800

    refactor(日志): 使用goexl/log库

---
+++ application.go
@@ -0,0 +1,7 @@
+package app
+
+import (
+	"gitea.com/ruijc/app/internal/core"
+)
+
+type Application = core.Application
---
+++ id.go
@@ -0,0 +1,7 @@
+package app
+
+import (
+	"gitea.com/ruijc/app/internal/core"
+)
+
+type Id = core.Id
